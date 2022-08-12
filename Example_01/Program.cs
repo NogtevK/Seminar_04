5db6d5c98484da72b293bf72ba39db353b817898
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-12 10:13:35 +0300

    task 24 complete,test ok,add commit and push

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 
+*/
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i <= A; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
