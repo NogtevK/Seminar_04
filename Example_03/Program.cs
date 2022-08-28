975c2d49925669ffdc8b48fb380afef10ebacb57
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-28 05:38:49 +0300

    Ex_3 Complete

--- Example_03/Program.cs
+++ Example_03/Program.cs
@@ -11,7 +11,7 @@ string text = "— Я думаю, — сказал князь, улыбаясь,
 // string s = qwerty
 //            012345
 // s[3] // r
-string Replace(string text, char oldValue, char newValue);
+string Replace(string text, char oldValue, char newValue)
 
 {
     string result = String.Empty;
@@ -20,9 +20,13 @@ string Replace(string text, char oldValue, char newValue);
     for (int i = 0; i < length; i++)
     {
         if (text[i] == oldValue) result = result + $"{newValue}";
-        else (result = result + $"{text[i]}");
+        else result = result + $"{text[i]} ";
     }
     return result;
 }
 string newText = Replace(text, 'к', 'К');
 Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
