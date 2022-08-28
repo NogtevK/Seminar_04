868db13b2c9c7819527ecf502648d292e7e18bca
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-28 09:52:45 +0300

    Task 24,complete. Programm ok, commit and push

---
+++ Example_05/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 */
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i <= A; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
