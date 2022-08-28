79041a681c34f26918791d8bf61f51b900644d22
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-28 13:22:19 +0300

    task 30 complete/commit and push

---
+++ Example_08/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+ */
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(0, 2);
+    }
+}
+int[] mass = new int[8];
+FillArray(mass);
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write(mass[i] + " ");
+    }
+    Console.WriteLine();
+}
+PrintArray(mass);
+PrintArray(mass);
