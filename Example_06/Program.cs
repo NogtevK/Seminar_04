c32dc635f5867682f668e883aa51a98959d96aa2
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-28 11:22:22 +0300

    task 26 complete<commit and push

---
+++ Example_06/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+ */
+
+int func(int num)
+{
+    int count = 0;
+    for (int i = 0; num > i;)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine(func(456));
+Console.WriteLine(func(78));
+Console.WriteLine(func(458966));
+
+
+
+/* Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+int num = 0;
+while (i > 0)
+{
+    i = i / 10;
+    num++;
+}
+Console.WriteLine(num); */
