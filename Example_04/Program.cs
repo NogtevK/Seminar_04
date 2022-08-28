1224d97f4d5ad699561787b1402ee30f5d7b9137
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-28 06:15:56 +0300

    complete Ex_04, commit and push

--- Example_04/Program.cs
+++ Example_04/Program.cs
@@ -5,15 +5,37 @@
 3. Повторять пока есть не отсортированные
 элементы 
 */
-int[] arr = { 1, 6, 5, 37, 8, 0, 3, 4, 9 };
-
+int[] arr = { 1, 6, 5, 7, 8, 0, 3, 4, 9 };
 void PrintArray(int[] array)
 {
     int count = array.Length;
+
     for (int i = 0; i < count; i++)
     {
         Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
-void SelectionSort();
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
